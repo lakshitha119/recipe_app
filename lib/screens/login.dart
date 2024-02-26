30f32622b8772afd6711701769cdea0436e377eb
Lakshitha6737@gmail.com <racingandsports.lakshitha@gmail.com>
2024-02-26 23:26:46 +0530

    remove login screen from stack

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -59,7 +59,7 @@ class _LoginState extends State<Login> {
         final SharedPreferences prefs = await SharedPreferences.getInstance();
         prefs.setString("userid", res["results"]["id"]);
         prefs.setString("email", res["results"]["email"]);
-        Navigator.push(
+        Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (_) => BottomTabBar()));
       } else {
         MyToast.showError("Login Failed!");
